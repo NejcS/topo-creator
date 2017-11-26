e9ec6434c1dfa1de57c4792df76606c389255540
Nejc Stebe <nejc.stebe92@gmail.com>
2017-11-26 17:06:19 +0100

    refactored code and added straight line drawing mode

--- script.js
+++ script.js
@@ -1,102 +1,158 @@
-var canvas = new fabric.Canvas('canvas');
+var topoEditor = {
+    isLineDrawingMode: false,
 
-var defaultStrokeWidth = 1;
+    settings: {
+        strokeWidth: 1,
+        straightLineStrokeWidth: 2,
+        straightLineColor: 'rgb(50, 50, 50)',
+        canvasBackgroundColor: 'rgb(250, 250, 250)'
+    },
 
-canvas.setDimensions({ width: 500, height: 800});
-canvas.hoverCursor = 'pointer';
+    init: function() {
+        var self = this;
+        this.canvas = new fabric.Canvas('canvas');
+        this.canvas.setDimensions({ width: 500, height: 800});
+        this.canvas.hoverCursor = 'pointer';
+        this.canvas.setBackgroundColor(this.settings.canvasBackgroundColor, this.canvas.renderAll.bind(this.canvas));
 
-canvas.setBackgroundColor('rgb(250, 250, 250)', canvas.renderAll.bind(canvas));
-
-canvas.on('object:scaling', function(e) {
-    // force object's stroke width to default value
-    var o = e.target;
-    if (o.getObjects) {
-        o.getObjects().forEach(function(path) {
-            path.strokeWidth = defaultStrokeWidth / o.scaleX;
-        });
-    } else {
-        o.strokeWidth = defaultStrokeWidth / o.scaleX;     
-    }
-});
-
-var onSymbolClick = function(symbol) {
-    fabric.loadSVGFromURL('./symbols/' + symbol + '.svg', function(objects, options) {   
-        if (objects && objects.length) {
-            if (objects.length > 1) {
-                var group = fabric.util.groupSVGElements(objects, options);
-                canvas.add(group)
-            } else {
-                canvas.add(objects[0]);
-            }
-        } else {
-            console.warn('couldn\'t load symbol: ' + symbol);
-        }
-    });
-}
-
-
-document.getElementById('draw').onclick = function() {
-    canvas.isDrawingMode = !canvas.isDrawingMode;
-
-    canvas.freeDrawingBrush.color = '#AAA';
-    canvas.freeDrawingBrush.width = 1.5;
-    canvas.freeDrawingBrush.strokeDashArray = [5];
-};
-
-
-document.getElementById('save-image').onclick = function() {
-    if (!fabric.Canvas.supports('toDataURL')) {
-        alert('This browser doesn\'t support serialization of canvas to an image');
-    }
-    else {
-        document.getElementById('canvas').toBlob(function(blob) {
-            saveAs(blob, "slika.png");
-        });
-    }
-};
-
-var _clipboard;
-document.onkeydown = function(e) {
-    var eventObj = window.event ? event : e;
-    // copy
-    if (eventObj.keyCode == 67 && (eventObj.ctrlKey || eventObj.metaKey)) {
-        canvas.getActiveObject().clone(function(cloned) {
-            _clipboard = cloned;
-        });
-    }
-    // paste
-    else if (eventObj.keyCode == 86 && (eventObj.ctrlKey || eventObj.metaKey)) {
-        _clipboard.clone(function(clonedObj) {
-            canvas.discardActiveObject();
-
-            clonedObj.set({
-                left: clonedObj.left + 10,
-                top: clonedObj.top + 10,
-                evented: true,
-            });
-            
-            if (clonedObj.type === 'activeSelection') {
-                // active selection needs a reference to the canvas.
-                clonedObj.canvas = canvas;
-                clonedObj.forEachObject(function(obj) {
-                    canvas.add(obj);
+        this.canvas.on('object:scaling', function(e) {
+            // force object's stroke width to default value
+            var o = e.target;
+            if (o.getObjects) {
+                o.getObjects().forEach(function(path) {
+                    path.strokeWidth = self.settings.defaultStrokeWidth / o.scaleX;
                 });
-                // this should solve the unselectability
-                clonedObj.setCoords();
             } else {
-                canvas.add(clonedObj);
+                o.strokeWidth = self.settings.defaultStrokeWidth / o.scaleX;     
             }
-            
-            _clipboard.top += 10;
-            _clipboard.left += 10;
-            canvas.setActiveObject(clonedObj);
         });
-    }
-};
+        this.bindUIHandlers();
+    },
 
-document.getElementById('debug').onclick = function() {
-    canvas.setDimensions({ width: 1000, height: 1000});
-};
+    bindUIHandlers: function() {
+        var self = this;
+        document.getElementById('draw-dashed').onclick = function() {
+
+            self.canvas.isDrawingMode = !self.canvas.isDrawingMode;
+
+            self.canvas.freeDrawingBrush.color = '#AAA';
+            self.canvas.freeDrawingBrush.width = 1.5;
+            self.canvas.freeDrawingBrush.strokeDashArray = [5];
+        };
+
+        document.getElementById('save-image').onclick = function() {
+            if (!fabric.Canvas.supports('toDataURL')) {
+                alert('This browser doesn\'t support serialization of canvas to an image');
+            }
+            else {
+                // maybe use the canvas object directly
+                document.getElementById('canvas').toBlob(function(blob) {
+                    saveAs(blob, "slika.png");
+                });
+            }
+        };
+
+        document.getElementById('debug').onclick = function() {
+            // self.canvas.setDimensions({ width: 1000, height: 1000});
+        };
+
+        var symbolElements = document.getElementsByClassName('symbol');
+        for(var i = 0; i < symbolElements.length; i++) {
+            symbolElements[i].onclick = function(e) {
+                fabric.loadSVGFromURL('./symbols/' + this.dataset.symbolType + '.svg', function(objects, options) {   
+                    if (objects && objects.length) {
+                        if (objects.length > 1) {
+                            var group = fabric.util.groupSVGElements(objects, options);
+                            self.canvas.add(group)
+                        } else {
+                            self.canvas.add(objects[0]);
+                        }
+                    } else {
+                        console.warn('couldn\'t load symbol: ' + this.dataset.symbolType);
+                    }
+                });
+            };
+        }
+
+        var clipboard;
+        document.addEventListener('keydown', function(e) {
+            if (e.keyCode === 67 && (e.metaKey || e.ctrlKey)) {
+                var activeObject = self.canvas.getActiveObject();
+                if (activeObject) {
+                    activeObject.clone(function(clone) {
+                        clipboard = clone;
+                    });
+                }
+            } else if (e.keyCode === 86 && (e.metaKey || e.ctrlKey)) {
+                if (clipboard) {
+                    clipboard.clone(function(clone) {
+                        self.canvas.discardActiveObject();
+
+                        clone.set({
+                            left: clone.left + 50,
+                            top: clone.top + 50,
+                            evented: true
+                        });
+
+                        if (clone.type === 'activeSelection') {
+                            clone.canvas = self.canvas;
+                            clone.forEachObject(function(object) {
+                                self.canvas.add(object);
+                            });
+                            clone.setCoords();
+                        } else {
+                            self.canvas.add(clone);
+                        }
+
+                        clipboard.top += 10;
+                        clipboard.left += 10;
+                        self.canvas.setActiveObject(clone);
+                    });
+                }
+            } else if (e.keyCode === 46 || e.keyCode === 8) {
+                var activeObject = self.canvas.getActiveObject();
+                if (activeObject) activeObject.remove();
+            }
+            // if ESC => unselect selected object
+        });
+
+        document.getElementById('draw-straight').onclick = function() {
+            self.isLineDrawingMode = !self.isLineDrawingMode;
+            if (self.isLineDrawingMode) {
+                var mouseDown, line
+                self.canvas.on('mouse:down', function(o) {
+                    mouseDown = true;
+                    var pointer = self.canvas.getPointer(o.e);
+                    var points = [pointer.x, pointer.y, pointer.x, pointer.y];
+
+                    line = new fabric.Line(points, {
+                        strokeWidth: self.settings.straightLineStrokeWidth,
+                        stroke: self.settings.straightLineColor,
+                        originX: 'center',
+                        originY: 'center'
+                    });
+                    self.canvas.add(line);
+                });
+                self.canvas.on('mouse:move', function(o) {
+                    self.canvas.renderAll();    // really neccessary?
+                    if (!mouseDown) return;
+                    var pointer = self.canvas.getPointer(o.e);
+                    line.set({ x2: pointer.x, y2: pointer.y });
+                    self.canvas.renderAll();
+                });
+                self.canvas.on('mouse:up', function(o) {
+                    var pointer = self.canvas.getPointer(o.e);  // what?!?
+                    mouseDown = false;
+                });
+            } else {
+                self.canvas.off('mouse:down', null);
+                self.canvas.off('mouse:move', null);
+                self.canvas.off('mouse:up', null);
+            }
+        };
+    }
+}
+topoEditor.init();
 
 /*
     // unused code / examples
